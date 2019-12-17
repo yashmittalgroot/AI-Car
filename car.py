007d36da5528e7e563cb0b821dbc5dba33f77ecb
yashmittalgroot <37185735+yashmittalgroot@users.noreply.github.com>
2019-12-17 18:39:07 +0530

    Removing some errors

--- car.py
+++ car.py
@@ -5,7 +5,6 @@ Created on Tue Dec 10 10:38:46 2019
 """
 
 from pygame.math import Vector2
-import numpy as np
 
 class Car:
     def __init__(self,x,y,angle=0.0,velocity = Vector2(3.0, 0.0),ppu=32,length=2,max_steering=30) :
@@ -18,15 +17,15 @@ class Car:
         self.ppu=ppu
         self.on=True
         self.is_finished=False
-        self.velocity = self.velocity.rotate(-self.angle)
+        #self.velocity = self.velocity.rotate(-self.angle)
 
     def update(self, dt):
         if self.on:
             if self.steering:
                 if(self.steering>0):
-                    self.angle+=2
+                    self.angle+=3
                 elif(self.steering<0):
-                    self.angle-=2
+                    self.angle-=3
             self.position += self.velocity.rotate(-self.angle) * dt 
         
     def reset(self,x,y,angle=0.0,velocity= Vector2(3.0, 0.0)):
@@ -35,4 +34,4 @@ class Car:
         self.angle =  angle
         self.steering = 0.0
         self.on=True
-        self.velocity = self.velocity.rotate(-self.angle)
+        #self.velocity = self.velocity.rotate(-self.angle)

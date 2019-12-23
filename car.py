4bf000d2f934d763c6263deb6c8f6ae07475a093
yashmittalgroot <37185735+yashmittalgroot@users.noreply.github.com>
2019-12-23 10:22:44 +0530

    Adding comment

--- car.py
+++ car.py
@@ -7,6 +7,7 @@ Created on Tue Dec 10 10:38:46 2019
 from pygame.math import Vector2
 
 class Car:
+    #Initalization
     def __init__(self,x,y,angle=0.0,velocity = Vector2(3.0, 0.0),ppu=32,length=2,max_steering=30) :
         self.position = Vector2(x, y)
         self.velocity = velocity
@@ -17,8 +18,8 @@ class Car:
         self.ppu=ppu
         self.on=True
         self.is_finished=False
-        #self.velocity = self.velocity.rotate(-self.angle)
-
+    
+    # Update position of centre of image of car
     def update(self, dt):
         if self.on:
             if self.steering:
@@ -27,11 +28,11 @@ class Car:
                 elif(self.steering<0):
                     self.angle-=3
             self.position += self.velocity.rotate(-self.angle) * dt 
-        
+    
+    # reset car
     def reset(self,x,y,angle=0.0,velocity= Vector2(3.0, 0.0)):
         self.position = Vector2(x, y)
         self.velocity = velocity
         self.angle =  angle
         self.steering = 0.0
         self.on=True
-        #self.velocity = self.velocity.rotate(-self.angle)
